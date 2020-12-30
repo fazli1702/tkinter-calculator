ac1ca2d5a48476ad8acfe0a83740742d4cbc7542
Fazli Yazid <fazliyazid1702@gmail.com>
2020-12-30 15:27:24 +0800

    improve aesthetics of GUI

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculator GUI
+
+This is a simple calculator gui created using python and tkinter module. I refered to this article for this project https://www.studytonight.com/tkinter/calculator-application-using-tkinter  
+
+![](image.png)
--- calculator.py
+++ calculator.py
@@ -3,6 +3,7 @@ import tkinter as tk
 win = tk.Tk()
 
 win.title('Calculator')
+win.geometry('330x343')
 win.resizable(0, 0)  # prevent resizing of window
 
 EXPRESSION = ''  # math expression on screen
@@ -41,72 +42,63 @@ def set_entry():
 # input text - display user math expression
 entry_text = tk.StringVar()
 
-entry = tk.Entry(width = 30, textvariable = entry_text)
-entry.grid(row = 0, column = 0, columnspan = 3)
+entry_frame = tk.Frame(win, width = 312, height = 50, bd = 0)
+entry_frame.pack(side = tk.TOP)
+
+entry = tk.Entry(entry_frame, font = ('arial', 18, 'bold') , width = 50, textvariable = entry_text, justify = tk.RIGHT)
+entry.grid(row = 0, column = 0)
+entry.pack(ipady = 10)
 
 # buttons
-button_clear = tk.Button(text = 'C', width = 20, height = 4, command = lambda: clear())
-button_clear.grid(row = 1, column = 0, columnspan = 2)
+padx, pady = 1, 1
 
-button_delete = tk.Button(text = u'\u232b', width = 10, height = 4, command = lambda: delete())
-button_delete.grid(row = 1, column = 2)
+button_frame = tk.Frame(win, width = 312, height = 272.5, bg = 'grey')
+button_frame.pack()
+
+button_clear = tk.Button(button_frame, text = 'C', width = 22, height = 3, bg = 'lightgrey', command = lambda: clear()).grid(row = 1, column = 0, columnspan = 2, padx = padx, pady = pady)
+
+button_delete = tk.Button(button_frame, text = u'\u232b', width = 10, height = 3, bg = 'lightgrey', command = lambda: delete()).grid(row = 1, column = 2, padx = padx, pady = pady)
 
 # row 2
 row = 2
-button_1 = tk.Button(text = '1', width = 10, height = 4, command = lambda: click(1))
-button_1.grid(row = row, column = 0)
+button_1 = tk.Button(button_frame, text = '1', width = 10, height = 3, bg = 'white', command = lambda: click(1)).grid(row = row, column = 0, padx = padx, pady = pady)
 
-button_2 = tk.Button(text = '2', width = 10, height = 4, command = lambda: click(2))
-button_2.grid(row = row, column = 1)
+button_2 = tk.Button(button_frame, text = '2', width = 10, height = 3, bg = 'white', command = lambda: click(2)).grid(row = row, column = 1, padx = padx, pady = pady)
 
-button_3 = tk.Button(text = '3', width = 10, height = 4, command = lambda: click(3))
-button_3.grid(row = row, column = 2)
+button_3 = tk.Button(button_frame, text = '3', width = 10, height = 3, bg = 'white', command = lambda: click(3)).grid(row = row, column = 2, padx = padx, pady = pady)
 
 # row 3
 row = 3
-button_4 = tk.Button(text = '4', width = 10, height = 4, command = lambda: click(4))
-button_4.grid(row = row, column = 0)
+button_4 = tk.Button(button_frame, text = '4', width = 10, height = 3, bg = 'white', command = lambda: click(4)).grid(row = row, column = 0, padx = padx, pady = pady)
 
-button_5 = tk.Button(text = '5', width = 10, height = 4, command = lambda: click(5))
-button_5.grid(row = row, column = 1)
+button_5 = tk.Button(button_frame, text = '5', width = 10, height = 3, bg = 'white', command = lambda: click(5)).grid(row = row, column = 1, padx = padx, pady = pady)
 
-button_6 = tk.Button(text = '6', width = 10, height = 4, command = lambda: click(6))
-button_6.grid(row = row, column = 2)
+button_6 = tk.Button(button_frame, text = '6', width = 10, height = 3, bg = 'white', command = lambda: click(6)).grid(row = row, column = 2, padx = padx, pady = pady)
 
 # row 4
 row = 4
-button_7 = tk.Button(text = '7', width = 10, height = 4, command = lambda: click(7))
-button_7.grid(row = row, column = 0)
+button_7 = tk.Button(button_frame, text = '7', width = 10, height = 3, bg = 'white', command = lambda: click(7)).grid(row = row, column = 0, padx = padx, pady = pady)
 
-button_8 = tk.Button(text = '8', width = 10, height = 4, command = lambda: click(8))
-button_8.grid(row = row, column = 1)
+button_8 = tk.Button(button_frame, text = '8', width = 10, height = 3, bg = 'white', command = lambda: click(8)).grid(row = row, column = 1, padx = padx, pady = pady)
 
-button_9 = tk.Button(text = '9', width = 10, height = 4, command = lambda: click(9))
-button_9.grid(row = row, column = 2)
+button_9 = tk.Button(button_frame, text = '9', width = 10, height = 3, bg = 'white', command = lambda: click(9)).grid(row = row, column = 2, padx = padx, pady = pady)
 
 # row 5
 row = 5
-button_0 = tk.Button(text = '0', width = 20, height = 4, command = lambda: click(0))
-button_0.grid(row = row, column = 0, columnspan = 2)
+button_0 = tk.Button(button_frame, text = '0', width = 22, height = 3, bg = 'white', command = lambda: click(0)).grid(row = row, column = 0, columnspan = 2, padx = padx, pady = pady)
 
-button_dot = tk.Button(text = '.', width = 10, height = 4, command = lambda: click('.'))
-button_dot.grid(row = row, column = 2)
+button_dot = tk.Button(button_frame, text = '.', width = 10, height = 3, bg = 'white', command = lambda: click('.')).grid(row = row, column = 2, padx = padx, pady = pady)
 
 # operators
 col = 3
-button_equal = tk.Button(text = '=', width = 10, height = 4, command = lambda: get_answer())
-button_equal.grid(row = 5, column = col)
+button_equal = tk.Button(button_frame, text = '=', width = 10, height = 3, bg = 'lightgrey', command = lambda: get_answer()).grid(row = 5, column = col, padx = padx, pady = pady)
 
-button_plus = tk.Button(text = '+', width = 10, height = 4, command = lambda: click('+'))
-button_plus.grid(row = 4, column = col)
+button_plus = tk.Button(button_frame, text = '+', width = 10, height = 3, bg = 'lightgrey', command = lambda: click('+')).grid(row = 4, column = col, padx = padx, pady = pady)
 
-button_minus = tk.Button(text = '-', width = 10, height = 4, command = lambda: click('-'))
-button_minus.grid(row = 3, column = col)
+button_minus = tk.Button(button_frame, text = '-', width = 10, height = 3, bg = 'lightgrey', command = lambda: click('-')).grid(row = 3, column = col, padx = padx, pady = pady)
 
-button_plus = tk.Button(text = '*', width = 10, height = 4, command = lambda: click('*'))
-button_plus.grid(row = 2, column = col)
+button_multiply = tk.Button(button_frame, text = '*', width = 10, height = 3, bg = 'lightgrey', command = lambda: click('*')).grid(row = 2, column = col, padx = padx, pady = pady)
 
-button_plus = tk.Button(text = u'\u00F7', width = 10, height = 4, command = lambda: click('/'))
-button_plus.grid(row = 1, column = col)
+button_divide = tk.Button(button_frame, text = u'\u00F7', width = 10, height = 3, bg = 'lightgrey', command = lambda: click('/')).grid(row = 1, column = col, padx = padx, pady = pady)
 
 win.mainloop()
